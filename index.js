7db852fefa822b34a5bc01e4a8a897eefa2b3c8a
Syndicated <sndctd@fbi.ac>
2023-04-22 15:55:23 +0100

    we don't get no errors anymore!!

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const client = new Discord.Client({
     Discord.GatewayIntentBits.GuildMessages,
     Discord.GatewayIntentBits.GuildMembers,
     Discord.GatewayIntentBits.GuildPresences,
-    Discord.GatewayIntentBits.MessageContent
+    Discord.GatewayIntentBits.MessageContent,
   ],
   ws: {
     properties: {
@@ -73,7 +73,29 @@ async function doAICompletion(message) {
       console.log(answer);
 
       messages.push({ role: "assistant", content: answer });
-      message.channel.send(answer);
+
+      function splitMessage(str, size) {
+        const numChunks = Math.ceil(str.length / size);
+        const chunks = new Array(numChunks);
+
+        for (let i = 0, c = 0; i < numChunks; ++i, c += size) {
+          const chunk = str.substr(c, size);
+
+          if (chunk.startsWith("```") && !chunk.endsWith("```")) {
+            chunks[i] = `${chunk}\`\`\``;
+          } else if (chunk.endsWith("```") && !chunk.startsWith("```")) {
+            chunks[i] = `\`\`\`${chunk}`;
+          }
+        }
+
+        return chunks;
+      }
+
+      const messageChunks = splitMessage(answer, 1920);
+
+      for (const chunk of messageChunks) {
+        await message.channel.send(chunk);
+      }
     } catch (error) {
       console.log(error);
 
@@ -111,34 +133,62 @@ client.on("messageCreate", async (message) => {
       }. the people in this server are: ${toOxfordComma([
         ...guildMembers.map((member) => member.user.username),
       ])}. the online members in this server are: ${toOxfordComma([
-        ...guildMembers.filter(member => member.presence && member.presence.status !== "offline").map((member) => member.user.username),
+        ...guildMembers
+          .filter(
+            (member) => member.presence && member.presence.status !== "offline"
+          )
+          .map((member) => member.user.username),
       ])}. the offline members in this server are: ${toOxfordComma([
-        ...guildMembers.map((member) => member.user.username).filter(username => !(guildMembers.filter(member => member.presence && member.presence.status !== "offline").map((member) => member.user.username)).includes(username)),
+        ...guildMembers
+          .map((member) => member.user.username)
+          .filter(
+            (username) =>
+              !guildMembers
+                .filter(
+                  (member) =>
+                    member.presence && member.presence.status !== "offline"
+                )
+                .map((member) => member.user.username)
+                .includes(username)
+          ),
       ])}
       YOU MUST TALK IN ALL LOWERCASE EXCEPT IF YOU ARE WRITING CODE, DO NOT SAY ANYTHING AFTER. IF A CODEBLOCK IS IN THE ANSWER, ANSWER WITH ONLY THE CODEBLOCK. if you understand, answer this: ${
         message.content
       }`,
     });
 
-    console.log(
-      {
-        role: "user",
-        content: `my name is ${message.author.username}. my discord tag is ${
-          message.author.tag
-        }. you are in a channel called #${
-          message.channel.name
-        }. the people in this server are: ${toOxfordComma([
-          ...guildMembers.map((member) => member.user.username),
-        ])}. the online members in this server are: ${toOxfordComma([
-          ...guildMembers.filter(member => member.presence && member.presence.status !== "offline").map((member) => member.user.username),
-        ])}. the offline members in this server are: ${toOxfordComma([
-          ...guildMembers.map((member) => member.user.username).filter(username => !(guildMembers.filter(member => member.presence && member.presence.status !== "offline").map((member) => member.user.username)).includes(username)),
-        ])}
+    console.log({
+      role: "user",
+      content: `my name is ${message.author.username}. my discord tag is ${
+        message.author.tag
+      }. you are in a channel called #${
+        message.channel.name
+      }. the people in this server are: ${toOxfordComma([
+        ...guildMembers.map((member) => member.user.username),
+      ])}. the online members in this server are: ${toOxfordComma([
+        ...guildMembers
+          .filter(
+            (member) => member.presence && member.presence.status !== "offline"
+          )
+          .map((member) => member.user.username),
+      ])}. the offline members in this server are: ${toOxfordComma([
+        ...guildMembers
+          .map((member) => member.user.username)
+          .filter(
+            (username) =>
+              !guildMembers
+                .filter(
+                  (member) =>
+                    member.presence && member.presence.status !== "offline"
+                )
+                .map((member) => member.user.username)
+                .includes(username)
+          ),
+      ])}
         YOU MUST TALK IN ALL LOWERCASE EXCEPT IF YOU ARE WRITING CODE, DO NOT SAY ANYTHING AFTER. IF A CODEBLOCK IS IN THE ANSWER, ANSWER WITH ONLY THE CODEBLOCK. if you understand, answer this: ${
           message.content
         }`,
-      }
-    );
+    });
 
     // add to queue
     q.enqueue(() => doAICompletion(message));
