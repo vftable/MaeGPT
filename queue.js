3c62f7a07666cba573c27c65828fc0cb6cd60142
Syndicated <sndctd@fbi.ac>
2023-04-22 12:22:34 +0100

    i fixed :skull:

--- queue.js
+++ queue.js
@@ -3,27 +3,32 @@ class Queue {
     this.items = [];
     this.isProcessing = false;
   }
-  
+
   enqueue(item) {
     this.items.push(item);
-    
+
     if (!this.isProcessing) {
       this.dequeue();
     }
   }
-  
+
   async dequeue() {
     this.isProcessing = true;
-    
+
     while (this.items.length > 0) {
-      const item = await this.items.shift();
+      const item =
+        (await this.items.shift()) ||
+        (() => {
+          return null;
+        });
+      console.log(item);
       await item();
     }
-    
+
     this.isProcessing = false;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
